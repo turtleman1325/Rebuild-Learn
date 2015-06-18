e58ded4e0a939ff83bdfa78b4cd3aaa780cb27fc
Art <art19ad4@gmail.com>
2015-06-18 10:56:23 -0700

    updated views removed faulty header/footer & previous/next links

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :sections
   resources :chapters
   resources :lessons
+
+  root 'lessons#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
